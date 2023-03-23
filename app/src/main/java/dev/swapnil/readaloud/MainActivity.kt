224e4b932a067d6bef39a210da262bd85a3f5e8d
SwapnilSonker <swapnilsonkarcse2019@bbdu.ac.in>
2023-03-23 09:59:50 +0530

    final changes made.

--- app/src/main/java/dev/swapnil/readaloud/MainActivity.kt
+++ app/src/main/java/dev/swapnil/readaloud/MainActivity.kt
@@ -1,146 +1,264 @@
 package dev.swapnil.readaloud
-
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
+import android.content.ActivityNotFoundException
 import android.net.Uri
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.text.method.ScrollingMovementMethod
+import android.speech.tts.UtteranceProgressListener
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.registerForActivityResult
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.isVisible
+
 import com.itextpdf.text.pdf.PdfReader
 import com.itextpdf.text.pdf.parser.PdfTextExtractor
 import dev.swapnil.readaloud.databinding.ActivityMainBinding
-import java.io.IOException
-import java.util.*
 
-class MainActivity : AppCompatActivity() {
+const val TAG = "MainActivity"
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
-    private lateinit var textToSpeech: TextToSpeech
-    private lateinit var outputTextView: TextView
-
-    private val READ_REQUEST_CODE = 42
-    private val PRIMARY = "primary"
-    private val LOCAL_STORAGE = "/storage/self/primary"
-    private val EXT_STORAGE = "/storage/self"
-
-    private val COLON = ":"
-
-    private lateinit var permissionlauncher: ActivityResultLauncher<Array<String>>
-    private var isReadPermissionGranted = false
-
+    lateinit var binding: ActivityMainBinding
+    lateinit var tts: TextToSpeech
+    lateinit var reader: PdfReader
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        binding.fabPlayTts.isEnabled = false
+        hideControls()
 
-        val outputTextView: TextView = findViewById(R.id.output_text)
-        outputTextView.movementMethod = ScrollingMovementMethod()
+        tts = TextToSpeech(this, this)
 
-        textToSpeech = TextToSpeech(applicationContext) { status ->
-            if (status == TextToSpeech.SUCCESS) {
-                textToSpeech.language = Locale.US
-            }
-        }
-
-
-        val getContent = registerForActivityResult(ActivityResultContracts.OpenDocument()){
-            uri: Uri? ->
-            permissionlauncher.launch(arrayOf(
-                "application/msword", //.doc (Microsoft Word)
-                "application/pdf" //.pdf (Pdf file)
-            ))
-        }
-        fun pickDocument() = getContent.launch(arrayOf("*/*"))
-
-
-
-        binding.fab.setOnClickListener { view ->
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-            intent.setType("*/*")
-            startActivityForResult(intent,READ_REQUEST_CODE)
-        }
-    }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
-        super.onActivityResult(requestCode, resultCode, resultData)
-        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            resultData?.data?.let { uri ->
-                // Toast.makeText(this, uri.path, Toast.LENGTH_SHORT).show()
-                uri.path?.let { Log.v("URI", it) }
-                readPdfFile(uri)
-            }
-        }
-    }
-
-
-    fun readPdfFile(uri: Uri?) {
-        uri?.path?.let { uriPath ->
-            val fullPath = if (uriPath.contains(PRIMARY)) {
-                LOCAL_STORAGE + uriPath.split(COLON)[1]
-            } else {
-                EXT_STORAGE + uriPath.split(COLON)[1]
-            }
-            Log.v("URI", "$uriPath $fullPath")
-            try {
-                PdfReader(fullPath).use { pdfReader ->
-                    val stringParser = PdfTextExtractor.getTextFromPage(pdfReader, 1).trim()
-                    outputTextView.text = stringParser
-                    textToSpeech.speak(stringParser, TextToSpeech.QUEUE_FLUSH, null, null)
+        tts.setOnUtteranceProgressListener(
+            object : UtteranceProgressListener() {
+                override fun onStart(p0: String?) {
+                    binding.fabPlayTts.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            this@MainActivity,
+                            R.drawable.ic_baseline_stop_24
+                        )
+                    )
                 }
-            } catch (e: IOException) {
-                e.printStackTrace()
+
+                override fun onStop(utteranceId: String?, interrupted: Boolean) {
+                    super.onStop(utteranceId, interrupted)
+                    binding.fabPlayTts.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            this@MainActivity,
+                            R.drawable.ic_baseline_play_arrow_24
+                        )
+                    )
+                }
+
+                override fun onDone(p0: String?) {
+                    binding.fabPlayTts.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            this@MainActivity,
+                            R.drawable.ic_baseline_play_arrow_24
+                        )
+                    )
+
+                    nextPageSpeak()
+                    //speak(binding.tvPageContent.text.toString())
+                }
+
+                override fun onError(p0: String?) {
+                    Toast.makeText(
+                        this@MainActivity, "Error : $p0", Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        )
+
+        val selectPdfResult = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            if (it?.let { it1 -> loadPdf(it1) } == true) {
+                setPageContent(1)
+            }
+        }
+
+        binding.fabSelectFile.setOnClickListener {
+            pickFile(selectPdfResult)
+        }
+
+        binding.fabNextPage.setOnClickListener {
+            nextPage()
+        }
+
+        binding.fabPreviousPage.setOnClickListener {
+            if (::reader.isInitialized) {
+                var currentPgNo = Integer.parseInt(binding.tvCurrentPgNo.text.toString())
+                if (1 < currentPgNo) {
+                    currentPgNo--
+                    setPageContent(currentPgNo)
+                    binding.tvCurrentPgNo.text = currentPgNo.toString()
+
+                    if (tts.isSpeaking) {
+                        tts.stop()
+                        speak(binding.tvPageContent.text.toString())
+                    }
+                }
+            }
+        }
+
+        binding.fabPlayTts.setOnClickListener {
+
+            if (::reader.isInitialized) {
+                if (tts.isSpeaking) {
+                    tts.stop()
+                    binding.fabPlayTts.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            this@MainActivity,
+                            R.drawable.ic_baseline_play_arrow_24
+                        )
+                    )
+
+                } else {
+                    speak(binding.tvPageContent.text.toString())
+
+                }
+            }
+        }
+
+        binding.fabAddBig.setOnClickListener {
+            pickFile(selectPdfResult)
+        }
+    }
+
+    //Changes to Next Page
+    private fun nextPageSpeak() {
+        if (::reader.isInitialized) {
+            var currentPgNo = Integer.parseInt(binding.tvCurrentPgNo.text.toString())
+            if (currentPgNo < reader.numberOfPages) {
+                currentPgNo++
+                setPageContent(currentPgNo)
+                binding.tvCurrentPgNo.text = currentPgNo.toString()
+
+                if (tts.isSpeaking) {
+                    tts.stop()
+                }
+                speak(binding.tvPageContent.text.toString())
             }
         }
     }
 
-    private fun <T : PdfReader, R> T.use(block: (T) -> R): R {
-        return try {
-            block(this)
-        } finally {
-            this.close()
+    //Changes to Next Page
+    private fun nextPage() {
+        if (::reader.isInitialized) {
+            var currentPgNo = Integer.parseInt(binding.tvCurrentPgNo.text.toString())
+            if (currentPgNo < reader.numberOfPages) {
+                currentPgNo++
+                setPageContent(currentPgNo)
+                binding.tvCurrentPgNo.text = currentPgNo.toString()
+
+                if (tts.isSpeaking) {
+                    tts.stop()
+                    speak(binding.tvPageContent.text.toString())
+                }
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    //Opens File Picker
+    private fun pickFile(selectPdfResult: ActivityResultLauncher<String>) {
+        if(tts.isSpeaking){
+            tts.stop()
+        }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        try {
+            selectPdfResult.launch("application/pdf")
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "No File Picker Found", Toast.LENGTH_SHORT).show()
+            Log.e(TAG, "pickFile: ${e.message}")
         }
     }
 
+    //Sets the reader
+    private fun loadPdf(uri: Uri): Boolean {
 
+        try {
+            reader = PdfReader(contentResolver.openInputStream(uri))
+
+            if (reader.numberOfPages == 0) {
+                Toast.makeText(this, "Empty Pdf", Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            showControls()
+
+            binding.tvTotalPages.text = reader.numberOfPages.toString()
+            binding.tvPgNoSeperator.text = "/"
+            binding.tvCurrentPgNo.text = "1"
+            return true
+
+        } catch (e: Exception) {
+            Toast.makeText(this, "Error While Reading PDF", Toast.LENGTH_SHORT).show()
+            Log.e(TAG, "loadPdf: ${e.message}")
+            return false
+        }
+
+    }
+
+    //Sets Text of Page to the TextView
+    private fun setPageContent(pageNo: Int) {
+
+        if (pageNo <= reader.numberOfPages) {
+            binding.tvPageContent.text = "Page $pageNo \n\n" + PdfTextExtractor.getTextFromPage(
+                reader,
+                pageNo
+            ).trim()
+        }
+    }
+
+    //For TextToSpeech Init
+    override fun onInit(p0: Int) {
+        binding.fabPlayTts.isEnabled = true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tts.shutdown()
+    }
+
+    //Speaks the text with TextToSpeech
+    private fun speak(text: String) =
+        tts.speak(text.trim(), TextToSpeech.QUEUE_FLUSH, null, "PdfReader")
+
+    private fun hideControls(){
+        binding.fabPlayTts.isVisible = false
+        binding.fabPreviousPage.isVisible = false
+        binding.fabNextPage.isVisible = false
+        binding.fabSelectFile.isVisible = false
+        binding.tvCurrentPgNo.isVisible = false
+        binding.tvPgNoSeperator.isVisible = false
+        binding.tvTotalPages.isVisible = false
+
+        binding.tvHeadTop.isVisible = false
+        binding.ivHeadTop.isVisible = false
+        binding.lineHeadTop.isVisible = false
+    }
+
+    private fun showControls(){
+
+        binding.fabAddBig.isVisible = false
+        binding.tvHead1.isVisible = false
+        binding.tvHead2.isVisible = false
+        binding.tvHead3.isVisible = false
+
+        binding.fabPlayTts.isVisible = true
+        binding.fabPreviousPage.isVisible = true
+        binding.fabNextPage.isVisible = true
+        binding.fabSelectFile.isVisible = true
+        binding.tvCurrentPgNo.isVisible = true
+        binding.tvPgNoSeperator.isVisible = true
+        binding.tvTotalPages.isVisible = true
+
+        binding.tvHeadTop.isVisible = true
+        binding.ivHeadTop.isVisible = true
+        binding.lineHeadTop.isVisible = true
+    }
 }
-
-
